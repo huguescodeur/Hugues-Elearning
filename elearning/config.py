703f55a7a771f10728f37b9912e81e8e63aefdac
huguescodeur <huguescodeur@gmail.com>
2024-07-22 01:15:10 +0000

    First commit

---
+++ elearning/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'django-insecure-=_pzkx5k80+25sp4=i*%netd=semvimp3#%_yk712fgz$scn*9'
+EMAIL_HOST_SENDER = 'goliyao09@gmail.com'
+EMAIL_HOST_RECEIVER = 'huguescodeur@gmail.com'
+EMAIL_HOST_PASSWORD = 'dqtylrxcwoflitis'
